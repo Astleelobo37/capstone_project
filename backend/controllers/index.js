3440f228074466e4d7643558320a6656f6eefa8a
Astleelobo37 <astleotho@gmail.com>
2025-04-01 20:24:55 +1300

    deleted typeController file and typeRoutes.js as it wasn't needed

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     userController: require('./userController'),
     maskController: require('./maskController'),
-    typeController: require('./typeController'),
     testResultController: require('./testResultController')
 }; 
